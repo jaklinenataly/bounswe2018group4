96c6239de6228d32385efe35e32bae61a6ad7f01
Emre Koç <emreh134150@gmail.com>
2018-04-12 15:03:46 +0300

    TwitterAPI projesinde allAPIs adlı app oluşturuldu

--- resources/TwitterAPI_Project/TwitterAPI/wsgi.py
+++ resources/TwitterAPI_Project/TwitterAPI/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for TwitterAPI project.
+WSGI config for untitled project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TwitterAPI.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "untitled.settings")
 
 application = get_wsgi_application()
